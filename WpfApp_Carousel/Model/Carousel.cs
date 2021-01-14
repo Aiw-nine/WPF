f1c9a197ee150cf1eaf64996a4f093da4d479f86
Aiw-nine <74449260+Aiw-nine@users.noreply.github.com>
2021-01-14 15:51:03 +0800

    Add files via upload

---
+++ WpfApp_Carousel/Model/Carousel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Carousel.Model
+{
+    public class Carousel
+    {
+        public string name { get; set; }
+        public string imgpath { get; set; }
+        public string info { get; set; }
+    }
+}
